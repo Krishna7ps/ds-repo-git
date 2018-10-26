5fadcc4dc0ad563580fbf9c24f2fe215157d3e22
krishna <krishna@email.com>
2018-10-26 09:08:07 +0530

    docker for kubernetes-App.js testing

--- docker/frontend/src/App.js
+++ docker/frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello Krishna
+            Hello Krishna, this is from mac
           </p>
           <a
             className="App-link"
