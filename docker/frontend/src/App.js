e5a7315eb64feb991ccbc4fc13ba2c6207e7aa88
krishna7ps <krishnaps909@gmail.com>
2018-10-26 04:13:48 +0000

    docker-compose file update

--- docker/frontend/src/App.js
+++ docker/frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Congratualtions!!, testing successful
+              "Testing to see if this is auto updated"
           </p>
           <a
             className="App-link"
